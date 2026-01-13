41c7d9f61d58c2eb4236b6d97678829173133455
anandaraj46 <218763745+anandaraj46@users.noreply.github.com>
2026-01-13 23:55:06 +0530

    modified gitignore and created secrets.example.h

---
+++ firmware/sEMG/secrets.example.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// -------- WiFi Credentials --------
+#define WIFI_SSID "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+// -------- MQTT Configuration --------
+#define MQTT_BROKER "broker.hivemq.com"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "gripmate/control"
---
+++ firmware/sEMG/secrets.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// -------- WiFi Credentials --------
+#define WIFI_SSID "GNXS-3F6790"
+#define WIFI_PASSWORD "chacko1186"
+
+// -------- MQTT Configuration --------
+#define MQTT_BROKER "broker.hivemq.com"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "gripmate/control"
